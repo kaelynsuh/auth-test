c155c560443b47d3830b154306c0e0a10385201a
Kaelyn Suh <kaelyn.suh@gmail.com>
2018-08-17 11:49:49 -0400

    Add routes and respond to requests for json

--- auth-test-backend/config/routes.rb
+++ auth-test-backend/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :examples
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope :api, defaults: {format: :json} do
+    resources :examples
+  end
 end
